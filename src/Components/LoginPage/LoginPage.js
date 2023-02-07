8d3920343ad59fa346be8329544af7c2224285c3
GodwinEgo <godwinehile04@gmail.com>
2023-02-08 00:39:28 +0100

    h

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -18,7 +18,7 @@ class LoginPage extends Component
             Hello
           </Grid>
           <Grid item xs={ 6 }>
-            <div >
+            <div className="loginpage_main" >
               <div>
                 <img src={ inst_image } width="450" />
               </div>
