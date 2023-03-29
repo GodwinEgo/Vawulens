41a737c5a61344418b5db4aab409a1ee1fc775c0
GodwinEgo <godwinehile04@gmail.com>
2023-03-30 00:23:42 +0100

    nav

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () =>
+{
+  return (
+    <nav>
+      <div className="container nav__container">
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
