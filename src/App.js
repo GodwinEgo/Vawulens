4c19860ee98058cc302c26ab9660e6914ebf9671
GodwinEgo <godwinehile04@gmail.com>
2023-03-29 23:51:24 +0100

    CleanUp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import './App.css';
-import LoginPage from './Components/LoginPage/LoginPage';
+import React from 'react'
 
-function App ()
+const App = () =>
 {
   return (
-    <div className="App">
-      <LoginPage />
+    <div>
+      HomePage
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
