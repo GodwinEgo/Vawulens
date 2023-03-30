33057210b83009af64f62b3036e72017ea971519
GodwinEgo <godwinehile04@gmail.com>
2023-03-30 12:46:32 +0100

    css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Images/logo.png'
 import { Link, NavLink } from 'react-router-dom'
@@ -8,13 +8,14 @@ import { CgMenuHotdog } from 'react-icons/cg'
 
 const Navbar = () =>
 {
+  const [ isNavShowing, setIsNavShowing ] = useState( false )
   return (
     <nav>
       <div className="container nav__container">
         <Link to='/' className='logo'>
           <img src={ logo } alt="Logo" />
         </Link>
-        <ul className="nav__links">
+        <ul className={ `nav__links ${ isNavShowing ? 'show__nav' : 'hide__nav' }` }>
           {
             links.map( ( { name, path }, index ) =>
             {
