d4e502a53345cfc08d54de2af354c9eac8db6ee6
GodwinEgo <godwinehile04@gmail.com>
2023-03-30 22:50:39 +0100

    header

--- src/Components/MainHeader.jsx
+++ src/Components/MainHeader.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../Pages/Home/Home.css';
+import CircleImage from '../Images/main_header.png';
 
 const MainHeader = () =>
 {
@@ -10,10 +12,15 @@ const MainHeader = () =>
           <h4>#100DaysOfWorkOut</h4>
           <h1>Join The Legends Of The Fitness World</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique harum, laudantium voluptates officiis rem optio.</p>
-
+          <Link to='/plans' className='btn lg'>
+            Get Started
+          </Link>
         </div>
         <div className="main__header-right">
-
+          <div className="main__header-circle"></div>
+          <div className="main__header-image">
+            <img src={ CircleImage } alt="CircleImage" />
+          </div>
         </div>
       </div>
     </header>
